077dd5bec885f8afc6a041cd13d51a37f4194101
mledwon4 <matt@iconlabs.co>
2014-11-05 21:57:15 -0500

    -Initial Commit

---
+++ SnapCam/SnapCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapCam
+//
+//  Created by Matt Ledwon on 11/5/14.
+//  Copyright (c) 2014 Matt Ledwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapCam/SnapCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapCam
+//
+//  Created by Matt Ledwon on 11/5/14.
+//  Copyright (c) 2014 Matt Ledwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
